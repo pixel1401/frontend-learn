f2fb2736140c84258ec14f62434800e9c096f9c4
pixsel1401 <pixsel1401@gmail.com>
2023-08-28 17:31:49 +0500

    fix unit test

--- src/pages/MainPage/ui/MainPage.test.tsx
+++ src/pages/MainPage/ui/MainPage.test.tsx
@@ -1,6 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import { Suspense } from 'react';
-import { Button } from 'shared/ui/Button/Button';
 import { renderWithTranslation } from 'shared/lib';
 import MainPage from './MainPage';
 import '@testing-library/jest-dom';
@@ -12,5 +10,4 @@ test('main page Test btn', () => {
     );
     const button = screen.getByText(/MainPage/i);
     expect(button).toBeInTheDocument();
-    expect(button).toMatchSnapshot('new');
 });
