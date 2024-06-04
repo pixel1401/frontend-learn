1f69fc690b0375072664f7083dd157687893523b
Erzhan <pixsel1401@gmail.com>
2024-06-04 16:43:04 +0500

    add notifications list

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,41 +1,38 @@
 import { classNames, Mods } from 'shared/lib/classNames/classNames';
-import React, { ReactNode } from 'react';
+import React, { memo, ReactNode } from 'react';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { useModal } from 'shared/lib/hooks/useModal/useModal';
 import { Overlay } from '../Overlay/Overlay';
+import cls from './Drawer.module.scss';
 import { Portal } from '../Portal/Portal';
-import cls from './Modal.module.scss';
 
-interface ModalProps {
+interface DrawerProps {
     className?: string;
-    children?: ReactNode;
+    children: ReactNode;
     isOpen?: boolean;
     onClose?: () => void;
     lazy?: boolean;
 }
 
-const ANIMATION_DELAY = 300;
-
-export const Modal = (props: ModalProps) => {
+export const Drawer = memo((props: DrawerProps) => {
     const {
         className,
         children,
-        isOpen,
         onClose,
+        isOpen,
         lazy,
     } = props;
+    const { theme } = useTheme();
 
     const {
         close,
         isClosing,
         isMounted,
     } = useModal({
-        animationDelay: ANIMATION_DELAY,
+        animationDelay: 300,
         onClose,
         isOpen,
     });
-
-    const { theme } = useTheme();
 
     const mods: Mods = {
         [cls.opened]: isOpen,
@@ -48,8 +45,8 @@ export const Modal = (props: ModalProps) => {
 
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
-                <Overlay onClick={close} />
+            <div className={classNames(cls.Drawer, mods, [className, theme, 'app_drawer'])}>
+                <Overlay onClick={onClose} />
                 <div
                     className={cls.content}
                 >
@@ -58,4 +55,4 @@ export const Modal = (props: ModalProps) => {
             </div>
         </Portal>
     );
-};
+});
